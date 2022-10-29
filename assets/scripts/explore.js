d1f4230376909b1208f70fe661769b4b66253180
anishrajeshkumar2002 <anishraj2002@gmail.com>
2022-10-29 15:09:30 -0700

    added js functionality for image and audio element

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -4,4 +4,6 @@ window.addEventListener('DOMContentLoaded', init);
 
 function init() {
   // TODO
+  const selectElement = document.querySelector('.ice-cream');
+
 }
--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -4,4 +4,28 @@ window.addEventListener('DOMContentLoaded', init);
 
 function init() {
   // TODO
+  const selectElement = document.getElementById('horn-select');
+  selectElement.addEventListener('change', (event) => {
+      console.log(event.target.value);
+      const name = event.target.value;
+      const yum =  document.querySelector("[alt='No image selected']");
+      const selectAudio = document.getElementsByClassName('hidden');
+      console.log(selectAudio);
+      if(name == "air-horn"){
+        yum.src = "assets/images/air-horn.svg";
+        selectAudio.src = "assets/audio/air-horn.mp3";
+      }
+      else if(name == "car-horn"){
+        yum.src = "assets/images/car-horn.svg";
+        selectAudio.src = "assets/audio/car-horn.mp3";
+      }
+      else if(name == "party-horn"){
+        yum.src = "assets/images/party-horn.svg";
+        selectAudio.src = "assets/audio/party-horn.mp3";
+      }
+      else{
+        yum.src = "assets/images/no-image.png";
+      }
+
+  });
 }
