38c1238d7457f3a101dd810ab0edc138dabab997
Chris Momil <khoolman1@gmail.com>
2022-10-29 15:43:50 -0700

    Add volume icon changing functionality for the slider, as well as audio adjustment tracking

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -5,11 +5,11 @@ window.addEventListener('DOMContentLoaded', init);
 function init() {
   // TODO
   const selectElement = document.getElementById('horn-select');
+  const selectAudio = document.getElementsByClassName('hidden');
   selectElement.addEventListener('change', (event) => {
       console.log(event.target.value);
       const name = event.target.value;
       const yum =  document.querySelector("[alt='No image selected']");
-      const selectAudio = document.getElementsByClassName('hidden');
       console.log(selectAudio);
       if(name == "air-horn"){
         yum.src = "assets/images/air-horn.svg";
@@ -26,6 +26,28 @@ function init() {
       else{
         yum.src = "assets/images/no-image.png";
       }
-
+  });
+  const inputElement = document.querySelector("[type='range']");
+  inputElement.addEventListener('change', (event) => {
+      console.log(inputElement);
+      const value = event.target.value;
+      const volumeImg =  document.querySelector("[alt='Volume level 2']");
+      if (value == 0) {
+        volumeImg.src = "assets/icons/volume-level-0.svg";
+        selectAudio.volume = value;
+      }
+      else if (value > 0 && value < 33) {
+        volumeImg.src = "assets/icons/volume-level-1.svg";
+        selectAudio.volume = value;
+      }
+      else if (value >= 33 && value < 67) {
+        volumeImg.src = "assets/icons/volume-level-2.svg";
+        selectAudio.volume = value;
+      }
+      else {
+        volumeImg.src = "assets/icons/volume-level-3.svg";
+        selectAudio.volume = value;
+      }
+      console.log(selectAudio.volume);
   });
 }
